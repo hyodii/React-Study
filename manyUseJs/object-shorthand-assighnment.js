5cbd20f5c05efcb0ac419885ab854e7834e1adab
HyoJin <hyojinj124@gmail.com>
2022-07-06 00:23:49 +0900

    WEATHER APP

---
+++ manyUseJs/object-shorthand-assighnment.js
@@ -0,0 +1,21 @@
+// object shorthand assighnment
+let name = "hyojin"
+let age = 17
+
+// let person = {
+//     name : name,
+//     age : age
+// }
+
+// 키와 값이 같으면 줄여 쓸수 있음!  name2 면 줄여서 쓸 수 없다!
+let person= {
+    name,
+    age
+}
+
+console.log(person)
+
+
+
+
+
